f44f8a7807de730bdc95d2108e1c50698e7fd731
gordon-v <gordon.velichkovski@proton.me>
2023-06-20 12:34:01 +0200

    Add project files.

---
+++ Scene.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTravel
+{
+    public class Scene
+    {
+        int Width;
+        int Height;
+
+        List<Star> stars;
+        public Scene(int Width, int Height) { 
+            this.Width = Width;
+            this.Height = Height;
+
+            stars = new List<Star>();
+        }
+
+        public void GenerateStars(int num)
+        {
+            Random r = new Random();
+            for(int i = 0; i < num; i++)
+            {
+                Star s = new Star(Width, Height,r);
+                stars.Add(s);
+            }
+        }
+
+        public void DrawScene(Graphics g)
+        {
+            g.Clear(Color.Black);
+            foreach (Star s in stars)
+            {
+                s.Draw(g);
+            }
+        }
+
+        //https://stackoverflow.com/questions/42477367/what-is-the-equivalent-of-map-from-processing-in-other-languages
+        public static double relativeMap(double value, double start1, double stop1, double start2, double stop2)    
+        {
+            return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+        }
+    }
+}
---
+++ Star.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTravel
+{
+    public class Star
+    {
+        private int Width;
+        private int Height;
+        public float X { set; get; }
+        public float Y { set; get; }
+        public float Z { set; get; }
+        public float radius { set; get; }
+
+        private Random r;
+
+        public Star(int Width, int Height, Random rand)
+        {
+            this.Width = Width;
+            this.Height = Height;
+
+
+            r = rand;
+
+            radius = 4;
+            Reset();
+        }
+        public void Reset()
+        {
+            X = r.Next(0, Width);
+            Y = r.Next(0, Height);
+            Z = r.Next(0, Width);
+        }
+        public void Update()
+        {
+            Z -= 10;
+            if (Z <= 0)
+            {
+                this.Reset();
+            }
+        }
+        public void Draw(Graphics g)
+        {
+            Brush b = new SolidBrush(Color.White);
+
+            this.Update();
+
+            float offsetX = 100.0f * (X / Z);
+            float offsetY = 100.0f * (Y / Z);
+            float scaleZ = 0.0001f * (2000.0f - Z);
+
+
+            g.FillEllipse(b, X - radius, Y - radius, radius * 2, radius * 2);
+            g.Flush();
+        }
+    }
+}
